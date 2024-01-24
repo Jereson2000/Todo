6e65a040452941fff4575004d1b09741801f69bc
Jere <jere.lotvonen@gmail.com>
2024-01-24 19:59:57 +0200

    draft for matching by length

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,30 @@ use std::io::{Error, Write};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let task = &args[1];
+    let task = "Mene töihin";
 
-    match add_task(&task) {
-        Ok(file) => file,
-        Err(error) => panic!("Problem generating a task: {}", error),
-    };
+    // TODO: Matching by arguments length
+    // https://doc.rust-lang.org/rust-by-example/std_misc/arg/matching.html
+
+    match args.len() {
+        _ => {
+            help();
+        }
+    }
+
+    let response = add_task(&task);
+
+    print!("{:?}", response)
 }
 
-fn add_task(task: &str) -> Result<(), Error> {
+fn add_task(task: &str) -> Result<String, Error> {
     let mut output = File::create("tasks.txt")?;
 
-    write!(output, "{task}")?;
+    write!(output, "{task}\n")?;
 
-    Ok(())
+    Ok(String::from("Successfully added task!\n"))
+}
+
+fn help() {
+    println!("Help in progress!")
 }
