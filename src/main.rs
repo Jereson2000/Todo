16bc614eaf9926935516708378be186a77572acd
Jere <jere.lotvonen@gmail.com>
2024-01-25 15:45:08 +0200

    initial match by argument length

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,31 @@ use std::io::{Error, Write};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let task = "Mene töihin";
 
-    // TODO: Matching by arguments length
     // https://doc.rust-lang.org/rust-by-example/std_misc/arg/matching.html
 
     match args.len() {
+        1 => {
+            help();
+        }
+        2 => {
+            help();
+        }
+        3 => {
+            let command = args[1].as_str();
+            let argument = args[2].as_str();
+            let result = match command {
+                "add" => add_task(&argument),
+                "list" => Ok(help()),
+                "rm" => Ok(help()),
+                _ => Ok(help()),
+            };
+            println!("{:?}", result);
+        }
         _ => {
             help();
         }
     }
-
-    let response = add_task(&task);
-
-    print!("{:?}", response)
 }
 
 fn add_task(task: &str) -> Result<String, Error> {
@@ -25,9 +36,10 @@ fn add_task(task: &str) -> Result<String, Error> {
 
     write!(output, "{task}\n")?;
 
-    Ok(String::from("Successfully added task!\n"))
+    Ok(String::from("Successfully added a task!"))
 }
 
-fn help() {
-    println!("Help in progress!")
+// TODO: implement help function so that it know how to answer to different inputs. 
+fn help() -> String {
+    return String::from("Help in progress!");
 }
