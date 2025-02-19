ddc2f7551b899b3395a1893e79e96081ae837811
jereson2000 <jere.lotvonen@gmail.com>
2025-02-19 18:59:07 +0200

    working app

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use std::u8;
-
 use clap::{Parser, Subcommand};
 use sqlite::Connection;
 
 #[derive(Parser)]
 #[command(name = "Todo")]
 #[command(version = "0.1")]
-#[command(about = "Todo list cli tool for listing tasks!", long_about = None)]
+#[command(about = "Todo cli tool for task management!", long_about = None)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
@@ -14,35 +12,31 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    // Lists tasks
     Add { task: String },
     List,
-    Done { id: u8 },
+    Delete { id: Vec<u8> },
 }
 
 fn main() {
-    // TODO: figure if there is a way to store the arguments as global constant as they won't be change after initialization.
-    // TODO: Determine the size for the array and initialize with it.
-    // Create re-sizable array for arguments and parse them.
     let cli = Cli::parse();
 
-    // TODO: move the database to a file.
-    // TODO: think how the connection should be handled
-    let conn: Connection = sqlite::open("./test.db").unwrap();
-    let statement =
-        String::from("CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY, task TEXT);");
+    let conn: Connection = sqlite::open("/var/db/todo/tasks.db").unwrap();
+    let statement = String::from(
+        "CREATE TABLE IF NOT EXISTS tasks(
+            id INTEGER PRIMARY KEY,
+            task TEXT);",
+    );
     conn.execute(statement).unwrap();
 
     handle_calls(cli, conn);
 }
 
 fn add_tasks(task: String, conn: Connection) {
-    let statement: String = format!("INSERT INTO tasks(id, task) VALUES(1, '{task}');");
+    let statement: String = format!("INSERT INTO tasks(task) VALUES('{task}');");
     conn.execute(statement).unwrap();
     println!("Added task: {task}");
 }
 
-// TODO: implement this better
 fn list_tasks(conn: Connection) {
     let statement = String::from("SELECT * FROM tasks;");
     conn.iterate(statement, |row| {
@@ -57,17 +51,19 @@ fn list_tasks(conn: Connection) {
     .unwrap();
 }
 
-fn delete_tasks(id: u8, conn: Connection) {
-    let statement = format!("delete from tasks where id={id};");
-    conn.execute(statement).unwrap();
-    println!("Deleted task with id: {id}")
+fn delete_tasks(id: Vec<u8>, conn: Connection) {
+    for i in id {
+        let statement = format!("DELETE FROM tasks WHERE id=={i};");
+        conn.execute(statement).unwrap();
+        println!("Deleted task with id: {i}")
+    }
 }
 
 fn handle_calls(cli: Cli, conn: Connection) {
     match cli.command {
         Commands::Add { task } => add_tasks(task, conn),
 
-        Commands::Done { id } => delete_tasks(id, conn),
+        Commands::Delete { id } => delete_tasks(id, conn),
 
         Commands::List => list_tasks(conn),
     }
